ad91ca53e2ab5e47b915df0a5b77f85839a90330
Zieciu <jakziec@gmail.com>
2015-05-26 12:16:29 +0200

    original IHS

---
+++ scripts/FIHS.m
@@ -0,0 +1,21 @@
+function [FRGB]=FIHS(RGB, P)
+% save('RGB.mat', 'RGB');
+% save('P.mat', 'P');
+
+RGB=double(RGB);
+P=double(P);
+I=RGB(:,:,1)*1/3+RGB(:,:,2)*1/3+RGB(:,:,3)*1/3;
+
+%hist matching
+%P=P*std(std(I))/std(std(P));
+%P=P-(mean(mean(P))-mean(mean(I)));
+P=(P-mean(P(:)))*std(I(:))/std(P(:)) + mean(I(:));
+
+for n=1:3
+    FRGB(:,:,n)=RGB(:,:,n)+P-I;
+end
+%m= min(min(min(FRGB)));
+%if m<0
+%    FRGB=FRGB-m;
+%end    
+%FRGB=FRGB/max(max(max(FRGB(:,:,1:3))));
--- scripts/main.m
+++ scripts/main.m
@@ -2,3 +2,41 @@ clc;
 close all;
 clear all;
 
+% cd ../examples
+addpath ../examples
+load image1M.mat;
+filenameM=A;
+load image1P.mat;
+filenameP=B;
+% cd ..
+
+handles.M=filenameM;
+handles.P=filenameP;
+
+handles.M=double(handles.M);
+handles.Mnormed=handles.M;
+[n1,n2,n3]=size(handles.M);
+for i=1:n3
+    handles.Mnormed(:,:,i)=handles.M(:,:,i)/max(max(handles.M(:,:,i)));
+end
+% axes(handles.multi_axes);
+% plotmulti(handles);
+% axes(handles.pan_axes);
+
+handles.P=double(handles.P);
+handles.Pnormed=handles.P/max(max(handles.P));
+% plotpan(handles);
+% guidata(hObject,handles);
+
+%-----------------------------
+% addpath IHS
+% cd IHS
+fihs = FIHS(handles.M, handles.P);
+handles.F=FIHS(handles.M, handles.P);
+handles.Fnormed=FIHS(handles.Mnormed, handles.Pnormed);
+% cd ..
+
+handles.Fnormed=handles.Fnormed-min(min(min(handles.Fnormed)));
+handles.Fnormed=handles.Fnormed/max(max(max(handles.Fnormed)));
+
+imshow(handles.Fnormed);
